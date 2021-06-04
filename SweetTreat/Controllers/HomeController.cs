99bf794f56b86fa7641104f9b908c6e4af845b8a
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-06-04 09:42:55 -0800

    add splashpage in HomeController.cs andViews/Home/Index.html

--- SweetTreat/Controllers/HomeController.cs
+++ SweetTreat/Controllers/HomeController.cs
@@ -4,10 +4,20 @@ namespace SweetTreat.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SweetTreatContext _db;
+    public HomeController(SweetTreatContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary model = new Dictionary<string, List>();
+      List<Flavor> flavorList = _db.Flavors.ToList();
+      flavorList<Treat> treatList = _db.Treats.ToList();
+      model.Add("Flavors", flavorList);
+      model.Add("Treats", treatList);
+      return View(model);
     }
   }
 }
